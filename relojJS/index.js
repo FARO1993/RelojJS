1d5ccc0db98b12428dcc99c79cef2899897153b4
FARO1993 <facundo.m.rodriguez1993@gmail.com>
2022-05-03 19:38:00 -0300

    cambios en estilo y fondo dinamico

--- relojJS/index.js
+++ relojJS/index.js
@@ -1,23 +1,38 @@
 var display = document.getElementById("reloj");
 var fecha = document.getElementById("fecha");
 
-let dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+let dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 
 function mueveReloj() {
     var timeZone = new Date();
     var hora = timeZone.getHours();
     var minutos = timeZone.getMinutes();
     var segundos = timeZone.getSeconds();
+    //Concateno valores para mostrar el horario completo.
     display.value = hora + ":" + minutos + ":" + segundos;
 
+    //Dependiendo el horario, cambia fondo y fuentes.
     if( hora > 18 ) {
         document.body.style.backgroundImage = "url('images/noche.jpg')";
-    } else if (hora === 6) {
+        display.style.color = "white";
+        display.style.textShadow = "0 0 30px white";
+        fecha.style.color = "white";
+        fecha.style.textShadow = "0 0 30px white";
+        document.getElementById('titulo').style.color = "white";
+        document.getElementById('label1').style.color = "white";
+        document.getElementById('label2').style.color = "white"; 
+    } else if (hora > 6) {
         document.body.style.backgroundImage = "url('images/dia.jpg')";
+        display.style.color = "black";
+        fecha.style.color = "black";
+        document.getElementById('titulo').style.color = "black";
+        document.getElementById('label1').style.color = "black";
+        document.getElementById('label2').style.color = "black";
     }
 
     let dia = timeZone.getDay();
 
+    //Dependiendo del valor obtenido, corresponde a un día de la semana. 
     if( dia === 1 ) {
         dia = dias[0];
     } else if( dia === 2 ) {
@@ -37,15 +52,37 @@ function mueveReloj() {
     var fechaDia = timeZone.getDate();
     var fechaMes = timeZone.getMonth();
     var fechaAnio = timeZone.getFullYear();
-    fecha.value = `${dia} ${fechaDia}/${fechaMes}/${fechaAnio}`
+    //Concateno valores para mostrar la fecha completa.
+    fecha.value = `${dia} ${fechaDia}/${fechaMes}/${fechaAnio}`;
 
-
+    //Poner en marcha el reloj.
     setTimeout(() => {
         mueveReloj()
     }, 1000);
 
-    //console.log(timeZone.getDate())
-}
+};
 
-//console.log(document.getElementById(minutos))
+//Formulario para la alarma
+//Muestro el formulario
+function fijarAlarma() {
+    document.getElementById('alarma-form').style.display = 'block';
+    document.getElementById('boton-fijar').style.display = 'none';
+    document.getElementById('boton-guardar').style.display = 'block';
+    document.getElementById('boton-cancelar').style.display = 'block';
+};
+
+//Oculto y guardo los cambios
+function guardarAlarma() {
+    document.getElementById('alarma-form').style.display = 'none';
+    document.getElementById('boton-fijar').style.display = 'block';
+    document.getElementById('boton-guardar').style.display = 'none';
+    document.getElementById('boton-cancelar').style.display = 'none';
+};
+
+//Oculto el form sin guardar ningun cambio.
+function cancelar() {
+    guardarAlarma();       
+};
+
+
 
